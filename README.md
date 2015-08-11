0956db2a6031ab457c731b57e043f6ac392c1522
Juan Soto <juan@juansoto.me>
2015-08-11 02:11:40 -0500

    Adds text wrapping and cowsay

--- README.md
+++ README.md
@@ -12,13 +12,23 @@ npm install -g wwwtxt
 wwwtxt
 ```
 
-It will print a random tweet.
+Will print a random tweet.
+
+```
+wwwtxt --cow
+```
+
+Will print a random tweet using [cowsay](https://www.npmjs.com/package/cowsay)
 
 ## Why?
 
 So you can do this:
 
 ![](https://i.imgur.com/AIIq6rN.jpg)
+
+or this:
+
+![](http://i.imgur.com/Hd4Hi1F.jpg)
 
 ## Source
 [wwwtxt](https://twitter.com/wwwtxt)
--- source/index.js
+++ source/index.js
@@ -2,6 +2,10 @@
 
 import fs from 'fs';
 import path from 'path';
+import cowsay from 'cowsay';
+import monowrap from 'monowrap';
+
+const argv = require('yargs').argv;
 
 const tweets = JSON.parse(fs.readFileSync(path.join(__dirname, '../tweets.json'), 'utf-8'));
 
@@ -10,4 +14,15 @@ let random = tweets[Math.floor(Math.random() * tweets.length)];
 let message = random.tweet.slice(0, random.tweet.indexOf('☯') - 1);
 let date = random.tweet.slice(random.tweet.indexOf('☯') + 1);
 
-console.log(message, '-', date);
+// Wrap text so it looks nice
+let output = monowrap(message + '\n - ' + date, {
+  width: 30
+})
+
+if (argv.cow) {
+  console.log(cowsay.say({
+    text: output
+  }));
+} else {
+  console.log(output);
+}
